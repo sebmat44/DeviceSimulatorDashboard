e027e1deaeb0f98e3782f3763aef26a3935003d5
sebmat44 <forarch44@gmail.com>
2026-09-09 17:08:33 +0200

    chore: initial project setup with Avalonia MVVM template

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DeviceSimulatorDashboard.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DeviceSimulatorDashboard.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
