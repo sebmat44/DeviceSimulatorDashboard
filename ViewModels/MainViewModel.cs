34dc936afbf1e80296a5c1608fe9285db1090ae0
sebmat44 <forarch44@gmail.com>
2026-09-11 18:14:14 +0200

    feat: data binding and button command added

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,9 +1,20 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input; // Wymagane dla obsługi przycisków!
 
 namespace DeviceSimulatorDashboard.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
+    // Nasza zmienna statusu
     [ObservableProperty]
-    public partial string Greeting { get; set; } = "Welcome to Avalonia!";
+    public partial string ConnectionStatus { get; set; } = "Status: Odłączony";
+
+    // Nasza logika przycisku
+    [RelayCommand]
+    private void ConnectToDevice()
+    {
+        // Zmieniamy wartość naszej właściwości. 
+        // Dzięki Data Bindingowi, UI zaktualizuje się SŁAM!
+        ConnectionStatus = "Status: Połączono po UDP!";
+    }
 }
